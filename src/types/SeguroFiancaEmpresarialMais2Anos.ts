312443066fef473fa79f979bf8eeb4193305f44a
Giovanni <giovannicruzvinhedo@gmail.com>
2024-10-07 11:28:38 -0300

    chore: SeguroFiancaEmpresarialMais2Anos type fixed

--- src/types/SeguroFiancaEmpresarialMais2Anos.ts
+++ src/types/SeguroFiancaEmpresarialMais2Anos.ts
@@ -1,35 +1,37 @@
 export type SeguroFiancaEmpresarialMais2Anos = {
   id: string;
+  id_numero: number;
   acao: "PENDENTE" | "FINALIZADO";
-  nome_imobiliaria_corretor: string;
   opcao_tributaria: "LUCRO REAL" | "LUCRO" | "PRESUMIDO" | "SIMPLES NACIONAL";
-  tipo_empresa:
-    | "LIMITADA"
-    | "INDIVIDUAL"
-    | "MICROEMPRESA"
-    | "PÚBLICA"
-    | "SEM FINS LUCRATIVOS";
   nome_empresa: string;
+  cnpj: string;
   telefone_empresa: string;
   email_empresa: string;
-  cnpj: string;
   atividade_economica: string;
-  valor_capital_social?: number;
+  capital_social?: number;
   faturamento_mensal?: number;
+  nome_socio_1?: string;
   cpf_socio_1?: string;
+  nome_socio_2?: string;
   cpf_socio_2?: string;
+  nome_socio_3?: string;
   cpf_socio_3?: string;
-  endereco_sede_atual: string;
+
+  cep: string;
+  endereco: string;
+  bairro: string;
+  numero_endereco: number;
+  complemento?: string;
+  cidade: string;
+  estado: string;
+
   motivo_locacao:
     | "ABERTURA DE FILIA"
     | "TROCA DO LOCAL"
     | "DE SEDE"
     | "LOCAÇÃO PARA MORADIA";
   tipo_imovel: "PRÓPRIO" | "ALUGADO";
-  valor_locacao_pretendida: number;
-  valor_conta_agua?: number;
-  valor_conta_energia?: number;
-  valor_aluguel_atual?: number;
+  valor_aluguel: number;
   nome_locador_imobiliaria: string;
   telefone: number;
   created: Date;
