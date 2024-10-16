46230861ce90f28b193b861560d2516d7d64d795
Giovanni <giovannicruzvinhedo@gmail.com>
2024-10-16 01:48:04 -0300

    fix: realtime quadro de anotacoes

--- src/utils/api/QuadroAnotacaoService.ts
+++ src/utils/api/QuadroAnotacaoService.ts
@@ -1,7 +1,8 @@
 import { QuadroAnotacao } from "@/types/QuadroAnotacao";
 import pb from "../backend/pb";
-import { ClientResponseError } from "pocketbase";
+import { ClientResponseError, RecordSubscription } from "pocketbase";
 
+// Função para buscar o Quadro de Anotação
 export async function fetchQuadroAnotacao(): Promise<QuadroAnotacao> {
   const id = "exdcxe57lff9p9o";
 
@@ -17,6 +18,7 @@ export async function fetchQuadroAnotacao(): Promise<QuadroAnotacao> {
   }
 }
 
+// Função para atualizar o Quadro de Anotação
 export async function updateQuadroAnotacao(
   data: Partial<QuadroAnotacao>
 ): Promise<QuadroAnotacao> {
@@ -32,4 +34,16 @@ export async function updateQuadroAnotacao(
     console.error("Erro ao atualizar o Quadro de Anotação:", err);
     throw new Error("Erro ao atualizar o Quadro de Anotação");
   }
+}
+
+// Função de assinatura para ouvir eventos de tempo real na coleção "quadro_anotacao"
+export function subscribeToQuadroAnotacaoUpdates(
+  callback: (e: RecordSubscription<QuadroAnotacao>) => void
+) {
+  pb.collection("quadro_anotacao").subscribe("*", callback);
+}
+
+// Função para cancelar a assinatura
+export function unsubscribeFromQuadroAnotacaoUpdates() {
+  pb.collection("quadro_anotacao").unsubscribe();
 }
