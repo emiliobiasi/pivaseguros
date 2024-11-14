fbb80f140d9ecac236addfacdd6c7ee9e6fc3247
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2024-11-14 00:03:34 -0300

    chore: EfetivacaoSeguroTable fixed

--- src/components/EfetivacaoSeguroFiancaTable/efetivacao-seguro-fianca-table.tsx
+++ src/components/EfetivacaoSeguroFiancaTable/efetivacao-seguro-fianca-table.tsx
@@ -299,7 +299,7 @@ export function EfetivacaoSeguroFiancaTable({ data }: TableContentProps) {
                     <TableCell className="px-3 py-2 lg:px-6 lg:py-3">
                       {seguro.nome_inquilino_1
                         ? seguro.nome_inquilino_1
-                        : "Inquilino não informado pela imobiliária"}
+                        : seguro.nome_inquilino_3}
                     </TableCell>
                     <TableCell className="px-3 py-2 lg:px-6 lg:py-3">
                       {new Date(seguro.created).toLocaleDateString()} -{" "}
