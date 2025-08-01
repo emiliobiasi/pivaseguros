cbbfdeb267670b18c0dc2cd834ad9452e36a4054
Giovanni <giovannicruzvinhedo@gmail.com>
2025-08-01 17:53:26 -0300

    refactor: ajustar formatação e estrutura do código nos componentes de SeguroFiancaEmpresarialMais2Anos

--- src/types/SeguroFiancaEmpresarialMais2Anos.ts
+++ src/types/SeguroFiancaEmpresarialMais2Anos.ts
@@ -1,10 +1,10 @@
 export type SeguroFiancaEmpresarialMais2Anos = {
-  id: string;
-  id_numero: number;
-  status: "EM ANÁLISE" | "APROVADO" | "REPROVADO";
-  acao: "PENDENTE" | "FINALIZADO";
+  id: string
+  id_numero: number
+  status: "EM ANÁLISE" | "APROVADO" | "REPROVADO"
+  acao: "PENDENTE" | "FINALIZADO"
 
-  nome_imobiliaria: string;
+  nome_imobiliaria: string
 
   tipo_empresa:
     | "LTDA"
@@ -12,57 +12,58 @@ export type SeguroFiancaEmpresarialMais2Anos = {
     | "S.A. CAPITAL ABERTO"
     | "S.A. CAPITAL FECHADO"
     | "SEM FINS LUCRATIVOS"
-    | "PÚBLICO"	
+    | "PÚBLICO"
 
-  opcao_tributaria: "LUCRO REAL" | "LUCRO PRESUMIDO" | "SIMPLES NACIONAL";
-  nome_empresa: string;
-  cnpj: string;
-  telefone_empresa: string;
-  email_empresa: string;
-  atividade_economica: string;
-  capital_social?: number;
-  faturamento_mensal?: number;
-  nome_socio_1?: string;
-  cpf_socio_1?: string;
-  nome_socio_2?: string;
-  cpf_socio_2?: string;
+  opcao_tributaria: "LUCRO REAL" | "LUCRO PRESUMIDO" | "SIMPLES NACIONAL"
+  nome_empresa: string
+  cnpj: string
+  telefone_empresa: string
+  email_empresa: string
+  atividade_economica: string
+  capital_social?: number
+  faturamento_mensal?: number
+  nome_socio_1?: string
+  cpf_socio_1?: string
+  nome_socio_2?: string
+  cpf_socio_2?: string
 
-  cep: string;
-  endereco: string;
-  bairro: string;
-  numero_endereco: number;
-  complemento?: string;
-  cidade: string;
-  estado: string;
+  cep: string
+  endereco: string
+  bairro: string
+  numero_endereco: number
+  complemento?: string
+  cidade: string
+  estado: string
 
-  cep_empresa: string;
-  endereco_empresa: string;
-  bairro_empresa: string;
-  numero_endereco_empresa: number;
-  cidade_empresa: string;
-  estado_empresa: string;
-  complemento_empresa?: string;
+  // DESABILITADO DO FORM NA ATUALIZAÇÃO DO DIA 01/08/2025
+  cep_empresa?: string
+  endereco_empresa?: string
+  bairro_empresa?: string
+  numero_endereco_empresa?: number
+  cidade_empresa?: string
+  estado_empresa?: string
+  complemento_empresa?: string
+  tipo_imovel?: "PRÓPRIO" | "ALUGADO"
+  valor_aluguel?: number
+  nome_locador_imobiliaria?: string
+  telefone?: string
 
   motivo_locacao:
     | "ABERTURA DE FILIAL"
     | "TROCA DO LOCAL"
     | "DE SEDE"
-    | "LOCAÇÃO PARA MORADIA";
-  tipo_imovel: "PRÓPRIO" | "ALUGADO";
-  valor_aluguel?: number;
-  nome_locador_imobiliaria?: string;
-  telefone?: string;
-  aluguel: number;
-  agua?: number;
-  energia?: number;
-  gas?: number;
-  condominio?: number;
-  iptu?: number;
-  danos_imovel: "SIM" | "NÃO";
-  multa_rescisao: "SIM" | "NÃO";
-  pintura_interna: "SIM" | "NÃO";
-  pintura_externa: "SIM" | "NÃO";
-  observacao?: string;
-  created: Date;
-  updated: Date;
-};
+    | "LOCAÇÃO PARA MORADIA"
+  aluguel: number
+  agua?: number
+  energia?: number
+  gas?: number
+  condominio?: number
+  iptu?: number
+  danos_imovel: "SIM" | "NÃO"
+  multa_rescisao: "SIM" | "NÃO"
+  pintura_interna: "SIM" | "NÃO"
+  pintura_externa: "SIM" | "NÃO"
+  observacao?: string
+  created: Date
+  updated: Date
+}
