657d8072474efc23b59a01b1ba7fb5e179072296
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2025-10-13 13:06:18 -0300

    feat(BoletoDownloadHistoricoPage): restructure layout to include HamburguerMenu

--- src/screens/BoletoDownloadHistoricoPage.tsx
+++ src/screens/BoletoDownloadHistoricoPage.tsx
@@ -1,10 +1,18 @@
-import { BoletoCard } from "@/components/boleto-card";
-import BoletosHistorico from "@/components/boletos-historico";
+import { BoletoCard } from "@/components/boleto-card"
+import BoletosHistorico from "@/components/boletos-historico"
+import { HamburguerMenu } from "@/components/HambuguerMenu/hamburguer-menu"
 
 export default function BoletoDownloadHistoricoPage() {
   return (
-    <BoletoCard>
-      <BoletosHistorico />
-    </BoletoCard>
-  );
+    <>
+      <div className="px-4 pt-4">
+        <div className="container mx-auto flex justify-start">
+          <HamburguerMenu />
+        </div>
+      </div>
+      <BoletoCard>
+        <BoletosHistorico />
+      </BoletoCard>
+    </>
+  )
 }
